cb48a226257245e74431fa3e3ed9287ffa7f5f3c
rhalbersma <rhalbersma@gmail.com>
2019-10-26 23:58:03 +0200

    refactor player.py

--- scripts/event.py
+++ scripts/event.py
@@ -14,24 +14,24 @@ import requests
 
 import kleier.utils
 
-def _parse_group(eid, gid, table) -> tuple:
-    event_table = (pd
-        .read_html(str(table), header=3)[0]
+def _tourn_table(eid: int, gid: int, table: str) -> tuple:
+    tourn_table = (pd
+        .read_html(table, header=3)[0]
         .assign(eid = eid, gid = gid)
         .rename(columns=lambda x: re.sub(r'(^\d+)', r'result\1', x))
     )
-    last_row = event_table.tail(1)
+    last_row = tourn_table.tail(1)
     received = str(last_row.iloc[0, 0])
     results_from = 'Results from: '
     if received.startswith(results_from):
-        event_table.drop(last_row.index, inplace=True)
+        tourn_table.drop(last_row.index, inplace=True)
 
-    rounds = list(event_table.filter(regex='result\d+'))
-    event_cross = (pd
-        .wide_to_long(event_table.filter(['#', 'eid', 'gid'] + rounds), ['result'], i='#', j='round')
+    rounds = list(tourn_table.filter(regex='result\d+'))
+    results = (pd
+        .wide_to_long(tourn_table.filter(['#', 'eid', 'gid'] + rounds), ['result'], i='#', j='round')
         .reset_index()
     )
-    event_table.drop(columns=rounds, inplace=True)
+    standings = tourn_table.drop(columns=rounds)
 
     header_rows = table.find('thead').find_all('tr')
     name_place_date = header_rows[0].find('th').text
@@ -52,14 +52,28 @@ def _parse_group(eid, gid, table) -> tuple:
     ]
     received = '' if not received.startswith(results_from) else received.split(results_from)[1]
 
-    event_index = pd.DataFrame(
+    events = pd.DataFrame(
         data   =[(eid,   gid,   name,   place,   date,   group,   W,   D,   L,   received)],
         columns=['eid', 'gid', 'name', 'place', 'date', 'group', 'W', 'D', 'L', 'received']
     )
 
-    return event_index, event_table, event_cross
+    return events, standings, results
 
-def download(eid) -> tuple:
+def download(eid: int) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    """Download a Stratego tournament table.
+
+    Args:
+        eid: the integral event identifier
+
+    Returns:
+        A tuple of three Pandas DataFrames.
+
+    Example:
+        # Download the 2019 World Championships
+        >>> events, standings, results = download(655)
+
+    """
+    assert 1 <= eid
     url = f'https://www.kleier.net/cgi/tourn_table.php?eid={eid}'
     response = requests.get(url)
     assert response.status_code == 200
@@ -68,12 +82,25 @@ def download(eid) -> tuple:
     return tuple(
         pd.concat(list(t), ignore_index=True, sort=False)
         for t in zip(*[
-            _parse_group(eid, gid, table)
+            _tourn_table(eid, gid, str(table))
             for gid, table in enumerate(tables)
         ])
     )
 
-def download_all(eids):
+def download_all(eids) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    """Download all Stratego tournament tables.
+
+    Args:
+        eids: a range of integeral event identifiers
+
+    Returns:
+        A tuple of three Pandas DataFrames.
+
+    Example:
+        # Download all Stratego tournaments that have been played as of October 26, 2019
+        >>> events, standings, results = download_all(range(1, 1 + 662))
+
+    """
     return tuple(
         pd.concat(list(t), ignore_index=True, sort=False)
         for t in zip(*[
@@ -82,7 +109,7 @@ def download_all(eids):
         ])
     )
 
-def parse_table(df: pd.DataFrame) -> pd.DataFrame:
+def parse_standings(df: pd.DataFrame) -> pd.DataFrame:
     columns = df.columns.to_list()
     return (df
         .loc[:, columns[-2:] + columns[:-2]]
@@ -101,7 +128,7 @@ def parse_table(df: pd.DataFrame) -> pd.DataFrame:
         .astype(dtype={column: float           for column in ['eff_games', 'buchholz', 'median']})
     )
 
-def parse_cross(df: pd.DataFrame) -> pd.DataFrame:
+def parse_results(df: pd.DataFrame) -> pd.DataFrame:
     return (df
         .rename(columns={'#' : 'rank1'})
         .fillna({'result': '0-'})
@@ -114,15 +141,24 @@ def parse_cross(df: pd.DataFrame) -> pd.DataFrame:
             rank2  = lambda x: x.result.str.slice(0, -1),
             result = lambda x: x.result.str.slice(   -1)
         )
-        .loc[:,['eid', 'gid', 'round', 'rank1', 'rank2', 'result']]
+        .loc[:, ['eid', 'gid', 'round', 'rank1', 'rank2', 'result']]
         .astype(dtype={column: int for column in ['rank1', 'rank2']})
     )
 
-def main():
-    event_index, event_table, event_cross = download_all(range(1, 1 + 662))
-    kleier.utils._save_dataset(event_index, 'event_index')
-    kleier.utils._save_dataset(event_table, 'event_table')
-    kleier.utils._save_dataset(event_cross, 'event_cross')
+def main(argv):
+    """Download all Stratego tournament tables and store them on disk.
+
+    Args:
+        argv: a range of integeral event identifiers, starting at 1
+
+    Returns:
+        None.
+
+    """
+    events, standings, results = download_all(range(1, 1 + int(argv[1])))
+    kleier.utils._save_dataset(events, 'events')
+    kleier.utils._save_dataset(standings, 'standings')
+    kleier.utils._save_dataset(results, 'results')
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv))
