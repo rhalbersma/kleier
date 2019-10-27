aef1aabfc6876c91f3e9799081a0856ea01211cc
rhalbersma <rhalbersma@gmail.com>
2019-10-27 22:20:15 +0100

    add results and remarks from events

---
+++ scripts/events.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+#          Copyright Rein Halbersma 2019.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+import re
+import sys
+
+import bs4
+import pandas as pd
+import requests
+from typing import Sequence, Tuple
+
+import kleier.utils
+
+def _event(eid: int, gid: int, table_header_rows: bs4.element.ResultSet) -> pd.DataFrame:
+    name_place_date = (table_header_rows[0]
+        .find('th')
+        .text
+        .split('\xa0\xa0\xa0\xa0\xa0\xa0')
+    )
+    name, place_date = ('', name_place_date[0]) if len(name_place_date) == 1 else name_place_date
+    place_and_date = place_date.split()
+    place = ' '.join(place_and_date[:-1])
+    date = pd.to_datetime(place_and_date[-1])
+    group, scoring = (table_header_rows[1]
+        .find('th')
+        .text
+        .split('\xa0\xa0')
+    )
+    group = group.split(': ')[1][:-1]
+    W, D, L = [
+        int(points)
+        for points in scoring.split(': ')[1].split()
+    ]
+    return pd.DataFrame(
+        data   =[(eid,   gid,   name,   place,   date,   group,   W,   D,   L)],
+        columns=['eid', 'gid', 'name', 'place', 'date', 'group', 'W', 'D', 'L']
+    )
+
+def _file(eid: int, results_from: str) -> Tuple[str]:
+    return re.split(r'(^.*)\s?(\d{4}-\d{2}-\d{2})\s(.*$)', results_from)[1:-1]
+
+def _tourn_table(eid: int, gid: int, table: bs4.element.Tag) -> pd.DataFrame:
+    return (pd
+        .read_html(str(table), header=[2, 3])[0]
+        .assign(
+            eid = eid, 
+            gid = gid
+        )
+        .pipe(lambda x: x.loc[:, x.columns.to_list()[-2:] + x.columns.to_list()[:-2]])
+    )
+
+def _standings(tourn_table: pd.DataFrame) -> pd.DataFrame:
+    return tourn_table.drop(list(tourn_table.filter(regex='Results')), axis='columns')
+
+def _results(tourn_table: pd.DataFrame) -> pd.DataFrame:
+    return (pd.wide_to_long(tourn_table
+                .filter(regex='eid|gid|#|Results')
+                .pipe(lambda x: x.set_axis(x.columns.to_flat_index().map(''.join), axis='columns', inplace=False)),
+            ['Results'], i='##', j='round'
+        )
+        .reset_index()
+    )
+
+def event_group_table(eid: int, gid: int, table: bs4.element.Tag) -> Tuple[pd.DataFrame]:
+    tourn_table = _tourn_table(eid, gid, table)
+    last_row = tourn_table.tail(1)
+    results_from, sep = str(last_row.iloc[0, 2]), 'Results from: '
+    if results_from.startswith(sep):
+        tourn_table.drop(last_row.index, inplace=True)
+        file_from, file_date, file_name = _file(eid, results_from.split(sep)[1])
+    else:
+        file_from = file_date = file_name = ''
+    event = (_event(eid, gid, table.find('thead').find_all('tr'))
+        .assign(
+            file_from = file_from,
+            file_date = file_date,
+            file_name = file_name
+        )
+    )
+    standings = _standings(tourn_table)
+    results = _results(tourn_table)
+    return event, standings, results
+
+def download(eid: int) -> Tuple[pd.DataFrame]:
+    assert 1 <= eid
+    url = f'https://www.kleier.net/cgi/tourn_table.php?eid={eid}'
+    response = requests.get(url)
+    assert response.status_code == 200
+    soup = bs4.BeautifulSoup(response.content, 'lxml')
+    tables = soup.find_all('table', {'summary': 'Stratego Tournament Cross-Table'})
+    events, standings, results = tuple(
+        pd.concat(list(t), ignore_index=True, sort=False)
+        for t in zip(*[
+            event_group_table(eid, gid, table)
+            for gid, table in enumerate(tables)
+        ])
+    )
+    remarks = pd.DataFrame(
+        data=[
+            (tables.index(remarks.find_previous('table')), remarks.text)
+            for remarks in soup.find_all('pre')
+        ],
+        columns=['gid', 'remarks']
+    )
+    events = (pd
+        .merge(events, remarks, how='outer')
+        .fillna({'remarks': ''})
+    )
+    return events, standings, results
+
+def download_all(eids: Sequence[int]) -> Tuple[pd.DataFrame]:
+    return tuple(
+        pd.concat(list(t), ignore_index=True, sort=False)
+        for t in zip(*[
+            download(eid)
+            for eid in eids
+        ])
+    )
+
+def format_standings(df: pd.DataFrame) -> pd.DataFrame:
+    return (df
+        .pipe(lambda x: x.set_axis(x.columns.to_flat_index().map('_'.join), axis='columns', inplace=False))
+        .rename(columns=lambda x: re.sub(r'(.+)_\1', r'\1', x))
+        .rename(columns=lambda x: x.lower())
+        .rename(columns=lambda x: x.replace('.', '_'))
+        .rename(columns={'#': 'rank'})
+        .astype(dtype={column: int             for column in ['rank', 'standings_score']})
+        .astype(dtype={column: float           for column in ['rating_value', 'rating_change']})
+        .astype(dtype={column: pd.Int64Dtype() for column in ['rating_value', 'rating_change']})
+        .astype(dtype={column: float           for column in ['rating_eff_games', 'standings_buchholz', 'standings_median']})
+    )
+
+def format_results(df: pd.DataFrame) -> pd.DataFrame:
+    return (df
+        .rename(columns={
+            '##'     : 'rank',
+            'Results': 'result'
+        })
+        .fillna({'result': '0-'})
+        .replace(
+            {'result': r'(\d+[+=-])[BW]'},
+            {'result': r'\1'},
+            regex=True
+        )
+        .assign(
+            opponent = lambda x: x.result.str.slice(0, -1),
+            result   = lambda x: x.result.str.slice(   -1)
+        )
+        .loc[:, ['eid', 'gid', 'round', 'rank', 'opponent', 'result']]
+        .astype(dtype={column: int for column in ['rank', 'opponent']})
+    )
+
+def main(max_eid: int) -> Tuple[pd.DataFrame]:
+    events, standings, results = download_all(range(1, 1 + max_eid))
+    standings = format_standings(standings)
+    results = format_results(results)
+    kleier.utils._save_dataset(events, 'events')
+    kleier.utils._save_dataset(standings, 'standings')
+    kleier.utils._save_dataset(results, 'results')
+    return events, standings, results
+
+if __name__ == '__main__':
+    sys.exit(main(int(sys.argv[1])))
---
+++ scripts/players.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+#          Copyright Rein Halbersma 2019.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+import re
+import sys
+
+import bs4
+import pandas as pd
+import requests
+from typing import Sequence, Tuple
+
+import kleier.utils
+
+def _header_name(header: bs4.element.Tag) -> str:
+    return header.text.split('History of ')[1]
+
+def _table_name(table: bs4.element.Tag) -> str:
+    return table.attrs['summary'].split('Game Balance of ')[1]
+
+def _player(pid: int, name: str) -> pd.DataFrame:
+    return pd.DataFrame(
+        data   =[(pid,   name)],
+        columns=['pid', 'name']
+    )
+
+def _games(pid: int, table: bs4.element.Tag) -> pd.DataFrame:
+    return (pd
+        .read_html(str(table), header=[1, 2])[0]
+        .assign(
+            pid = pid
+        )
+        .pipe(lambda x: x.loc[:, x.columns.to_list()[-1:] + x.columns.to_list()[:-1]])
+    )
+
+def download(pid: int) -> Tuple[pd.DataFrame]:
+    assert 1 <= pid
+    url = f'https://www.kleier.net/cgi/player.php?pid={pid}'
+    response = requests.get(url)
+    assert response.status_code == 200
+    soup = bs4.BeautifulSoup(response.content, 'lxml')
+    header = soup.find('h1')
+    table = soup.find('table')
+    assert header or not table
+    name = '' if not header else _header_name(header)
+    if table:
+        assert name == _table_name(table)
+    player = _player(pid, name)
+    games = None if not table else _games(pid, table)
+    return player, games
+
+def download_all(pids: Sequence[int]) -> Tuple[pd.DataFrame]:
+    return tuple(
+        pd.concat(list(t), ignore_index=True, sort=False)
+        for t in zip(*[
+            download(pid)
+            for pid in pids
+        ])
+    )
+
+def format_games(df: pd.DataFrame) -> pd.DataFrame:
+    return (df
+        .pipe(lambda x: x.set_axis(x.columns.to_flat_index().map('_'.join), axis='columns', inplace=False))
+        .rename(columns=lambda x: x.lower())
+        .rename(columns=lambda x: x.replace(' ', '_'))
+        .rename(columns=lambda x: re.sub(r'(date).+$', r'\1', x))
+        .astype(dtype={'event_date'     : 'datetime64[ns]'})
+        .astype(dtype={'opponent_rating': pd.Int64Dtype()})
+    )
+
+def main(max_pid: int) -> Tuple[pd.DataFrame]:
+    players, games = download_all(range(1, 1 + max_pid))
+    games = format_games(games)
+    kleier.utils._save_dataset(players, 'players')
+    kleier.utils._save_dataset(games, 'games')
+    return players, games
+
+if __name__ == '__main__':
+    sys.exit(main(int(sys.argv[1])))
